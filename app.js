20c63c2422ebfe8d24415805e47679f434cd8521
Yashwant Singh <yashwantsing333@gmail.com>
2021-11-06 04:48:33 +0530

    add start script

--- app.js
+++ app.js
@@ -158,7 +158,8 @@ app.use((err,req,res,next)=>{
     res.status(statusCode).render('error',{err}) ;
 })
 
+const port = process.env.PORT||3000 ;
 
-app.listen(3000, () => {
-    console.log("Listening on portal 3000");
+app.listen(port, () => {
+    console.log(`Listening on Portal ${port}`);
 })
